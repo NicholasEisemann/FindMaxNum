522a7f61de0c0a913910d9dd7fd75c3073141667
Nicolas Eisemann <71433649+NicholasEisemann@users.noreply.github.com>
2020-12-17 14:44:45 +0300

    new commit

---
+++ ex(classes)/ClassFirst.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_classes_
+{
+    class ClassFirst
+    {
+        public int FindMax(int first, int second)
+        {
+            int result; //Здесь хранится наибольшее число
+
+            if (first > second)
+            {
+                result = first;
+            }
+            else
+            {
+                result = second;
+            }
+
+            return result;
+        }
+
+    }
+
+}
---
+++ ex(classes)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_classes_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+
+            ClassFirst numberManipulator = new ClassFirst();
+
+
+            Console.WriteLine("Введите число - X");
+            string x = Console.ReadLine();
+            Console.WriteLine("Введите число - Y");
+            string y = Console.ReadLine();
+
+            int xInt = Convert.ToInt32(x);
+            int yInt = Convert.ToInt32(y);
+
+
+            result = numberManipulator.FindMax(xInt, yInt);
+
+            if (result == xInt)
+            {
+                Console.WriteLine($"Число: X-{xInt} > Y-{yInt}");
+            }
+            else
+            {
+                Console.WriteLine($"Число: X-{xInt} < Y-{yInt}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
